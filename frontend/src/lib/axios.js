c6dbcd3f6866c74cc6caa99dbff7ece3d1576c1d
Shubhendu Singh <shubhendusingh26@gmail.com>
2025-03-19 09:54:03 +0530

    updated the vercel.json file

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://mern-chat-app-seven-iota.vercel.app/api"; // Update this with your Vercel backend URL
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-  withCredentials: true,
+  baseURL: BASE_URL,
+  withCredentials: true, // Ensure cookies are sent (for sessions)
 });
